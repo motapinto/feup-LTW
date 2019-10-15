1310e9d44bd62612a4b6dac11e284f1d9f30fbbc
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-10-15 11:15:09 +0100

    php update

---
+++ PHP/labEx/sum2.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+         <?php
+
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+
+            function addNumbers($num1, $num2){
+                        
+                return $num1 + $num2;
+
+            }
+
+            echo "$num1 + $num2 = ". addNumbers($num1, $num2);
+            echo "<br><br>";
+            echo "<a href=form2.html>Click me </a>"
+         ?>
+        
+    </body>
+</html>
