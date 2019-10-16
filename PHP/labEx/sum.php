263990f948542dcfdf7108c649742fdf79dc37b6
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-10-16 09:27:33 +0100

    tp php

---
+++ PHP/labEx/sum.php
@@ -0,0 +1,14 @@
+<?php
+function sum($num1, $num2){
+    return $num1 + $num2;
+}
+
+echo sum($_GET['num1'], $_GET['num2']);
+
+?>
+
+<html>
+    <body>
+        <a href="form2.html">Anotha One</a>
+    </body>
+</html>
