666d363e7d4c050c7d7682a3cb0e1dda7b711547
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-10-23 10:35:02 +0100

    ex5 done

--- PHP/labEx/exercise5/news_item.php
+++ PHP/labEx/exercise5/news_item.php
@@ -1,24 +1,22 @@
 <?php
 
   $db = new PDO('sqlite:news.db');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
   $stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = ?');
   $stmt->execute(array($_GET['id']));
   $article = $stmt->fetch();
 
-?>
-
-  <h1>$article['title']</h1>
-  <p>$article['introduction']</p>
-
-<?php
 
   $stmt = $db->prepare('SELECT * FROM comments JOIN users USING (username) WHERE news_id = ?');
   $stmt->execute(array($_GET['id']));
   $comments = $stmt->fetchAll();
 
-  foreach( $comments as $comments) { ?>
-  
-    <?php }
+  foreach( $comments as $comments) { 
+    echo '<h3>'.$comments['username'].'</h3>';
+    echo '<p>'.$comments['text'].'</p>';
+    echo '<p>'.date('Y-m-d H:i:s', $comments['published']).'</p>';
+  }
 
 ?>
