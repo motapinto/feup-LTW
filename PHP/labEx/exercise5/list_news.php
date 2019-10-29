48e9561189bfafdf8e94527dd1eeded7d42ae1b6
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-10-29 16:13:14 +0000

    EX5 DONE

--- PHP/labEx/exercise5/list_news.php
+++ PHP/labEx/exercise5/list_news.php
@@ -17,7 +17,7 @@
   foreach( $articles as $article) { 
     echo '<h1>'.$article['title'].'</h1>';
     echo '<p>'.$article['introduction'].'</p>';
-    echo '<p>'.$article['username'].'</p>';
+    echo '<h3>'.$article['username'].'</h3>';
     echo '<div>'.date('Y-m-d H:i:s', $article['published']).'</div>';
   }
 ?>
--- PHP/labEx/exercise5/news_item.php
+++ PHP/labEx/exercise5/news_item.php
@@ -4,10 +4,17 @@
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+  if (!isset($_GET['id']))
+    die("No id!");
+
   $stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = ?');
   $stmt->execute(array($_GET['id']));
-  $article = $stmt->fetch();
 
+  while( $article = $stmt->fetch()) { 
+    echo '<h1>'.$article['title'].'</h3>';
+    echo '<h3>'.$article['introduction'].'</h3>';
+    echo '<p>'.$article['fulltext'].'</p>';
+  }
 
   $stmt = $db->prepare('SELECT * FROM comments JOIN users USING (username) WHERE news_id = ?');
   $stmt->execute(array($_GET['id']));
@@ -16,7 +23,7 @@
   foreach( $comments as $comments) { 
     echo '<h3>'.$comments['username'].'</h3>';
     echo '<p>'.$comments['text'].'</p>';
-    echo '<p>'.date('Y-m-d H:i:s', $comments['published']).'</p>';
+    echo '<p>'.date('d/m/Y H:i:s', $comments['published']).'</p>';
   }
 
 ?>
