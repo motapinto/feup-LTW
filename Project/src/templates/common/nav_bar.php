2a1096f85efd97b40c0976de5a1f0689e3052aa9
luis ramos <deco.luis@live.com.pt>
2019-11-06 15:39:26 +0000

    after tp

--- Project/src/templates/common/nav_bar.php
+++ Project/src/templates/common/nav_bar.php
@@ -1,10 +1,6 @@
 <?php function drawNavBar() { ?>
   <nav>
-<<<<<<< HEAD
       <a href="../listings/listings_all.php"><img class="logo" src="../../assets/logo.png" alt="logo" ></a>
-=======
-      <a href="../listings/listings_all.php"><img class="logo" src="../../assets/logoi.png" alt="logo"></a>
->>>>>>> 2119b28563973f026316bd8f3ae1ba238626e537
         <ul class="links">
             <li><a href="#">About</a></li>
             <li><a href="#">Buy</a></li>
