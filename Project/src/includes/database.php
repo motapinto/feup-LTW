e0bf0da67f16b827933d99bafc4d0ae3c4e7162e
luis ramos <deco.luis@live.com.pt>
2019-11-09 16:19:45 +0000

    done

--- Project/src/includes/database.php
+++ Project/src/includes/database.php
@@ -8,34 +8,30 @@
      * access it and getDB() to get the database connection.
      */
     class Database {
-        private static $instance = NULL;
-        private $db = NULL;
+        private static $instance = null;
+        private $db;
 
         private function __construct() {
             try {
-                $db = new PDO('sqlite:'.__DIR__.'/campus_rentals.db');
+              $this->db = new PDO('sqlite:'.__DIR__.'/campus_rentals.db');
+              $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+              $this->db->query('PRAGMA foreign_keys = ON');
             }
             catch (PDOException $e) {
                 echo 'Connection failed: ' . $e->getMessage();
                 exit;
             }
-            //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            print("1\n");
-            //$this->db->query('PRAGMA foreign_keys = ON');
-            print("2\n");
-            if (NULL == $this->db) 
+            if (!$this->db) 
                 throw new Exception("Failed to open database");
-            
-            print("2\n");
         }
 
         public function getDB() {
             return $this->db;
         }
 
-        static function instance() {
-            if (NULL == self::$instance) {
+        public static function instance() {
+            if (!self::$instance) {
                 self::$instance = new Database();
             }
             return self::$instance;
