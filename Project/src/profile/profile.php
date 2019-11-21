704997237b5d25e1337dd8ab3c186c2375a0e0b3
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-11-21 21:56:04 +0000

    update on profile

--- Project/src/profile/profile.php
+++ Project/src/profile/profile.php
@@ -5,7 +5,7 @@
     include('../templates/tpl_common.php');         // prints the initial and final part of the HTML document
     include('../templates/tpl_navBar.php');         // prints the menu in HTML
     include('../templates/tpl_profile.php');        // prints the user profile
-    include('../templates/tpl_profile-edit.php');   // prints de profile settings
+    //include('../templates/tpl_profile-edit.php');   // prints de profile settings
 
     if(!isset($_SESSION['id']))
       header('Location: ../listings/all_listings.php');
@@ -14,7 +14,7 @@
     draw_navBar();
     $user = userProfile($_SESSION['id']);        // get's current user
     draw_profile($user);
-    draw_profile_settings($user);
+    //draw_profile_settings($user);
     //draw_profile_overview($user);
     //draw_profile_comments($user, $comments);
     draw_footer()
--- Project/src/templates/tpl_profile.php
+++ Project/src/templates/tpl_profile.php
@@ -1,4 +1,5 @@
 <?php function draw_profile($user) { ?>
+<!--*********************** PROFILE SIDEMENU ***********************-->
     <div id="user">
         <section class="side-drawer">
             <!-- USER PHOTO -->
@@ -29,10 +30,10 @@
             <!-- SIDEBAR BUTTONS -->
             <div class="profile-userbuttons">
                 <a href="../properties/properties.php">
-                    <button type="button" class="profile-userbuttons-properties">Properties</button>
+                    <button type="button" class="circular-button">Properties</button>
                 </a>
                 <a href="#">
-                    <button type="button" class="profile-userbuttons-message">Message</button>
+                    <button type="button" class="circular-button">Message</button>
                 </a>
             </div>
 
@@ -41,11 +42,11 @@
                 <ul class="usermenu">
                     <li>
                         <i class="material-icons"> menu </i>
-                        <a href="#"> Overview </a>
+                        <button class='no-button' onclick='profileOverview();'> Overview </button>
                     </li>
                     <li>
                         <i class="material-icons"> settings </i>
-                        <a href="#"> Account Settings </a>
+                        <button class='no-button' onclick='profileSettings();'> Account Settings </button>
                     </li>
                     <li>
                         <i class="material-icons"> star_border </i>
@@ -60,97 +61,259 @@
         </section> 
     </div>
 
-    <style>  
-        body {
-            background-color: white;
-        }
+<!--*********************** PROFILE OVERVIEW ***********************-->
+    <div id='profile-overview-tab' class='selected-tab'>
+    </div>
 
 
-        .user a {
-            text-decoration: none;
-        }
+<!--*********************** PROFILE SETTINGS ***********************-->
+    <div id='profile-settings-tab' class='selected-tab'>
+        <h1 id='profile-settings-title'>Edit Profile</h4>
+        <form action='../actions/action_profile_change.php' method='POST'>
+            <div id='profile-setting-name' class='profile-setting-elem'>
+                <label> Name </label>
+                <div class='profile-settings-aux-div'>
+                    <input name='name' type='text' value='<?=$user['name']?>'>
+                    <a href="#"><i class="material-icons" style="color:rgb(176, 183, 187);">arrow_forward_ios</i></a>
+                </div>
+            </div>
 
-        .side-drawer {
-            position: absolute;
-            display: flex; /* or inline-flex */
-            flex-direction: column ;
-            width: 280px;
-            height: 500px; 
-            background-color: white;
-            padding: 50px;
-            margin: 50px;
-            border: solid 1px rgb(175, 175, 175);
-        }
+            <div id='profile-setting-email' class='profile-setting-elem'>
+                <label> Email </label>
+                <div class='profile-settings-aux-div'>
+                    <input name='email' type='email' value='<?=$user['email']?>'>
+                    <a href="#"><i class="material-icons" style="color:rgb(176, 183, 187);">arrow_forward_ios</i></a>
+                    <span id='profile-settings-msg-email'></span>
+                </div>
+            </div>
 
+            <div id='profile-setting-age' class='profile-setting-elem'>
+                <label> Age </label>
+                <div class='profile-settings-aux-div'>
+                    <input name='age' type='number' min='18' max='120' value='<?=$user['age']?>'>
+                    <a href="#"><i class="material-icons" style="color:rgb(176, 183, 187);">arrow_forward_ios</i></a>
+                </div>
+            </div>
 
-        .profile-photo {
-            margin-left: 4em;
-            border-radius: 50%;
-        }
+            <div id='profile-setting-password' class='profile-setting-elem'>
+                <label> Password </label>
+                <div class='profile-settings-aux-div'>
+                    <input id='password' type='password' value='' onkeyup='checkPass();'/>
+                    <a href="#"><i class="material-icons" style="color:rgb(176, 183, 187);">arrow_forward_ios</i></a>
+                </div>
+            </div>
 
-        .profile-photo a:hover{
-            text-decoration: underline;
-        }
-
-        .profile-photo a{
-            color: black;
-            font-family: "Montserrat", sans-serif;
-        }
-
-        .profile-name {
-            text-align: center;
-            color: black;
-            font-family: "Montserrat", sans-serif;
-        }
-
-        .profile-userbuttons {
-            display: flex;
-            flex-direction: row ;
-        }
-
-        .profile-userbuttons button{
-            display: flex;
-            border: 1px solid rgb(175,175,175);
-            background-color: rgb(39, 64, 89);
-            letter-spacing: 1px;
-            margin: 10px;
-            padding: 10px 20px;
-        }
-
-        .profile-usermenu {
-            display: flex; /* or inline-flex */
-            flex-direction: column ; 
-            margin: 10px;
-            border-top: solid 3px rgb(136, 189, 234);
-            border-bottom: solid 3px rgb(136, 189, 234);
-        }
-
-        .usermenu {
-            list-style-type: none;
-            padding-left: 10%;
-        }
-
-        .usermenu li {
-            padding-bottom: 8px;
-        }
-
-        .usermenu li a {
-            list-style-type: none;
-        }
-
-        .usermenu a{
-            font-size: 20px;
-            font-family: "Montserrat", sans-serif;
-            color: black;
-        }
-
-        .usermenu a:hover{
-            text-decoration: underline;
-        }
-
-        .upload-photo {
-            
-        }
-
-    </style>
+            <div id='profile-settings-confirm_password' class='profile-setting-elem'>
+                <label> Confirm password </label>
+                <div class='profile-settings-aux-div'>
+                    <input id='confirm_password' type='password' value='' onkeyup='checkPass();'/>
+                    <a href="#"><i class="material-icons" style="color:rgb(176, 183, 187);">arrow_forward_ios</i></a>
+                    <span id='profile-settings-msg-pass'></span>
+                </div>
+            </div>
+        </form>
+    </div>
 <?php } ?>
+
+<script>
+    function profileOverview() {
+        document.getElementById("profile-settings-tab").style.display = "none";
+        document.getElementById("profile-overview-tab").style.display = "display";
+    }
+
+    function profileSettings() {
+        document.getElementById("profile-overview-tab").style.display = "none";
+        document.getElementById("profile-settings-tab").style.display = "block";
+    }
+
+    function checkEmail() {
+        var xhttp = new XMLHttpRequest();
+        var asynchronous = true;
+
+        //if(!isset($_POST['email']) || !isset($_POST['name']) || !isset($_POST['age']) || !isset($_POST['password']))
+        
+        xhttp.open('GET', 'getcustomer.php?q='+str, asynchronous);
+        xhttp.send();
+
+        /*if(se der erro dentro da funcao acrtion_profile_change) {
+            document.getElementById('profile-settings-msg-email').innerHTML = 'Email already exists';
+            document.getElementById('profile-settings-submit').disabled = false;
+        }*/
+    }
+
+    function checkPass() {
+        if(document.getElementById('password').value.length == 0 ||
+            document.getElementById('password').value ==document.getElementById('confirm_password').value) {
+
+            document.getElementById('password').style.backgroundColor = 'white';
+            document.getElementById('password').style.border = 'solid 1px rgb(176, 183, 187)'
+            document.getElementById('confirm_password').style.backgroundColor = 'white';
+            document.getElementById('confirm_password').style.border = 'solid 1px rgb(176, 183, 187)'
+            document.getElementById('profile-settings-msg-pass').innerHTML = '';
+            document.getElementById('profile-settings-submit').disabled = false;
+        }
+        else {
+            document.getElementById('confirm_password').style.backgroundColor  = 'rgb(246, 220, 220)';
+            document.getElementById('confirm_password').style.border = 'solid 1px rgb(233, 76, 76)'
+            document.getElementById('profile-settings-msg-pass').innerHTML = 'The password\'s dont\'t match';
+            document.getElementById('profile-settings-msg-pass').style.color = 'red';
+            document.getElementById('profile-settings-submit').disabled = true;
+        }
+    }
+
+    function submitForm(){
+        let name = document.getElementById('profile-setting-name').innerHTML.value;
+        let email = document.getElementById('profile-setting-email').innerHTML.value;
+        let age = document.getElementById('profile-setting-age').innerHTML.value;
+        let password = document.getElementById('profile-setting-password').innerHTML.value;
+        let confirm_password = document.getElementById('profile-setting-confirm_password').innerHTML.value;
+
+        let requestName = split(name, ' ');
+        requestName = requestName.join('+');
+        let requestEmail = email.replace('@', '%40');
+        let request = 'name=' + requestName + '&' + 'email=' + requestEmail + '&' + 'age=' + age + '&'+ 'password=' + password + '&' + 'control=' + confirm_password;
+        
+        let xhttp = new XMLHttpRequest();
+        let asynchronous = true;
+
+        // Define what happens on successful data submission
+        xhttp.addEventListener('load', function(event) {
+            alert('Yeah! Data sent and response loaded.');
+        });
+
+        // Define what happens in case of error
+        xhttp.addEventListener('error', function(event) {
+            alert('Oops! Something goes wrong.');
+        });
+
+        xhttp.open('POST', '../actions/action_profile_change.php?'+request, asynchronous);
+        xhttp.send();
+    }
+</script>
+
+<style>
+/************************* PROFILE SIDEMENU *************************/
+    .side-drawer {
+        position: absolute;
+        display: flex; /* or inline-flex */
+        flex-direction: column ;
+        width: 280px;
+        height: 500px; 
+        background-color: white;
+        padding: 50px;
+        margin: 50px;
+        border: solid 1px rgb(175, 175, 175);
+    }
+
+    .profile-photo {
+        margin-left: 4em;
+        border-radius: 50%;
+    }
+
+    .profile-photo a:hover{
+        text-decoration: underline;
+    }
+
+    .profile-photo a{
+        color: black;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    .profile-name {
+        text-align: center;
+        color: black;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    .profile-userbuttons {
+        display: flex;
+        flex-direction: row ;
+    }
+
+    .profile-userbuttons button{
+        display: flex;
+        border: 1px solid rgb(175,175,175);
+        background-color: rgb(39, 64, 89);
+        letter-spacing: 1px;
+        margin: 10px;
+        padding: 10px 20px;
+    }
+
+    .profile-usermenu {
+        display: flex; /* or inline-flex */
+        flex-direction: column ; 
+        margin: 10px;
+        border-top: solid 3px rgb(136, 189, 234);
+        border-bottom: solid 3px rgb(136, 189, 234);
+    }
+
+    .usermenu {
+        list-style-type: none;
+        padding-left: 10%;
+    }
+
+    .usermenu li {
+        padding-bottom: 8px;
+    }
+
+    .usermenu li a {
+        list-style-type: none;
+    }
+
+    .usermenu a{
+        font-size: 20px;
+        font-family: "Montserrat", sans-serif;
+        color: black;
+    }
+
+    .usermenu a:hover{
+        text-decoration: underline;
+    }
+
+    .upload-photo {  
+    }
+/*************************      COMMON      *************************/
+    .selected-tab {
+        display: none;
+        background-color: white;
+        width: 400px;
+        height: 600px;
+        margin-left: 600px;
+        padding: 40px;
+    }
+
+/************************* PROFILE OVERVIEW *************************/
+    /*a*/
+
+/************************* PROFILE SETTINGS *************************/
+    #profile-settings-title{
+        color: grey;
+    }
+
+    #profile-settings-tab input {
+        border: solid rgb(176, 183, 187) 1px;
+        color: grey;
+        width: 350px;
+        text-align: center;
+        padding-top: 15px;
+        border-radius: 5px;
+    }
+
+    #profile-settings-tab input:focus {
+        border: solid rgb(176, 183, 187) 2px;
+    }
+
+    .profile-settings-aux-div{
+        margin-bottom: 25px;            
+    }
+
+    .profile-setting-elem {
+        margin-bottom: 25px;      
+        border-bottom: solid 1px rgb(136, 189, 234);   
+    }
+
+    .profile-setting-elem:last-child {
+        border: none;
+    }
+</style>
+
