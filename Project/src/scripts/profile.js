5e2a5aaa65b5c752e8fa3a9e7a1e232c9b355fa9
luis ramos <deco.luis@live.com.pt>
2019-11-27 10:33:22 +0000

    end class

--- Project/src/scripts/profile.js
+++ Project/src/scripts/profile.js
@@ -27,8 +27,8 @@ function profileSecurity() {
 }
 
 function checkPass() {
-    if(document.getElementById('password').value.length == 0 ||
-        document.getElementById('password').value ==document.getElementById('confirm_password').value) {
+    if (document.getElementById('password').value.length == 0 ||
+        document.getElementById('password').value == document.getElementById('confirm_password').value) {
 
         document.getElementById('password').style.backgroundColor = 'white';
         document.getElementById('password').style.border = 'solid 1px rgb(176, 183, 187)'
@@ -36,9 +36,8 @@ function checkPass() {
         document.getElementById('confirm_password').style.border = 'solid 1px rgb(176, 183, 187)'
         document.getElementById('profile-settings-msg').innerHTML = '';
         document.getElementById('profile-settings-submit').disabled = false;
-    }
-    else {
-        document.getElementById('confirm_password').style.backgroundColor  = 'rgb(246, 220, 220)';
+    } else {
+        document.getElementById('confirm_password').style.backgroundColor = 'rgb(246, 220, 220)';
         document.getElementById('confirm_password').style.border = 'solid 1px rgb(233, 76, 76)'
         document.getElementById('profile-settings-msg').innerHTML = 'The password\'s don\'t match';
         document.getElementById('profile-settings-msg').style.color = 'red';
@@ -47,23 +46,17 @@ function checkPass() {
 }
 
 function encodeForAjax(data) {
-    return Object.keys(data).map(function(k){
+    return Object.keys(data).map(function(k) {
         return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
     }).join('&')
 }
 
-function decodeForAjax(data) {
-    return Object.keys(data).reduce(function(k){
-        return decodeURIComponent(k) + '=' + decodeURIComponent(data[k])
-    })
-}
-
-function submitForm(option){
+function submitForm(option) {
     let xhttp = new XMLHttpRequest();
     let asynchronous = true;
     let request, name, email, age, password;
 
-    switch(option) {
+    switch (option) {
         // user name
         case 0:
             name = document.getElementById('name').value;
@@ -71,13 +64,13 @@ function submitForm(option){
             requestName = requestName.join('+');
             request = 'name=' + requestName;
             break;
-        // user email
+            // user email
         case 1:
             email = document.getElementById('email').value;
             let requestEmail = email.replace('@', '%40');
             request = 'email=' + requestEmail;
             break;
-        // user age
+            // user age
         case 2:
             age = document.getElementById('age').value;
             request = 'age=' + age;
@@ -89,32 +82,31 @@ function submitForm(option){
 
     // Define what happens on successful data submission
     xhttp.addEventListener('load', function(event) {
-        //deencodeForAjax()
-        switch(option) {
+
+        switch (option) {
             // user name
             case 0:
                 document.getElementById('icon-name').className = 'fas fa-check';
-            // user email
+                // user email
             case 1:
                 emailAfter = document.getElementById('email').value;
-                if(email != emailAfter) {
+                if (email != emailAfter) {
                     document.getElementById('profile-settings-msg-email').innerHTML = '';
-                }
-                else {
+                } else {
                     document.getElementById('email').value = email;
                     document.getElementById('profile-settings-msg-email').innerHTML = 'Email already exists';
                     document.getElementById('profile-settings-msg-email').style.color = 'red';
                     document.getElementById('icon-name').className = 'fas fa-times';
                 }
                 break;
-            
-            //user age
+
+                //user age
             case 2:
                 document.getElementById('icon-age').className = 'fas fa-check';
 
-        default:
-            break;
-    }
+            default:
+                break;
+        }
     });
 
     // Define what happens in case of error
@@ -122,6 +114,6 @@ function submitForm(option){
         alert('Oops! Something goes wrong.');
     });
 
-    xhttp.open('POST', '../actions/action_profile_change.php?'+request, asynchronous);
+    xhttp.open('POST', '../actions/action_profile_change.php?' + request, asynchronous);
     xhttp.send();
 }
