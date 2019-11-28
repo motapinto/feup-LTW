3a8be203c3a7e1aa556b057a8314e7f7706ba815
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-11-28 22:35:05 +0000

    before laig

--- Project/src/templates/tpl_profile.php
+++ Project/src/templates/tpl_profile.php
@@ -140,7 +140,7 @@
         <section id='profile-sendMessage-tab' class='selected-tab'>
             <h1 id='profile-settings-title'>Edit Profile</h1>
                 <form id='profile-sendMessage' class='profile-setting-elem' action="../actions/action_message_add.php" method="get">
-                    <label for="sendMessage"></label>
+                    <label for="sendMessage"></label> <br>
                     <textarea name="sendMessage" id="sendMessage" cols="10" rows="10"></textarea> 
                     <input name="receiver" type="number" value='<?=$user['id']?>' style="display: none">
                     <button class="no-button"> Send Message </button>
