11315bc16f3f244b9182d87b7801f0f52b23a5ad
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-11-28 21:26:43 +0000

    messages start

---
+++ Project/src/database/messages.php
@@ -0,0 +1,50 @@
+<?php
+    include_once('../includes/database.php');      // connects to the database
+
+    // returns all messages from user with id=senderId to user with id=receiverId
+    function getAllMessagesFromUser($receiverId, $senderId) {
+        $db = Database::instance()->db();
+
+        $stmt = $db->prepare('SELECT * FROM Message WHERE receiver = ? AND sender = ?');
+        $stmt->execute(array($receiverId, $senderId));
+        $messages = $stmt->fetchAll();
+
+        if (!$messages) 
+            return -1;
+        
+        else 
+            return $messages;
+    }
+
+    // True if user with id=receiverId has received a message from user with id=senderId
+    function haveExchangeMessages($receiverId, $senderId) {
+        $db = Database::instance()->db();
+
+        $stmt = $db->prepare('SELECT * FROM Message WHERE receiver = ? AND sender = ?');
+        $stmt->execute(array($receiverId, $senderId));
+        $messages = $stmt->fetchAll();
+
+        if (!$messages) 
+            return false;
+        
+        else 
+            return true;
+    }
+
+    // Adds a new message to the database
+    function addMessage($receiverId, $senderId, $message, $date) {
+        $db = Database::instance()->db();
+
+        $stmt = $db->prepare('INSERT INTO Message (
+                message,
+                receiver,
+                sender,
+                date
+            )
+            VALUES (?, ?, ?, ?);
+        ');
+        $stmt->execute(array($message, $receiverId, $senderId, $date));
+        $user = $stmt->fetch();
+        return !$user?true:false;
+    }
+?>
