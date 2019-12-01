f2f638085a0b41cad64e01fbcea628bf18f8dba9
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-12-01 11:13:27 +0000

    test messages css + messages db update

--- Project/src/database/messages.php
+++ Project/src/database/messages.php
@@ -20,6 +20,26 @@
             return $messages;
     }
 
+    function getLastMessageFromConversation($user1, $user2) {
+        $db = Database::instance()->db();
+
+        $stmt = $db->prepare('SELECT message FROM Message WHERE 
+                              receiver = ? AND sender = ? OR
+                              receiver = ? AND sender = ?
+                              ORDER BY date LIMIT 1');
+        $stmt->execute(array($user1, $user2, $user2, $user1));
+        $message = $stmt->fetch();
+
+        // have not exchange messages
+        if (!$message) 
+            return -1;
+        
+        // last message to display in messages sidebar menu
+        else 
+            return $message;
+    }
+
+    // returns all users id that have exchange messages with user
     function getAllMessengers($user) {
         $db = Database::instance()->db();
 
@@ -35,10 +55,11 @@
         $stmt->execute(array($user, $user));
         $users = $stmt->fetchAll();
 
-        // have not exchange any messages
+        // have not exchange any messages with any user
         if (!$users) 
             return -1;
         
+        //  return users id's
         else 
             return $users;
     }
@@ -60,19 +81,3 @@
         return !$message?true:false;
     }
 ?>
-
-<!-- Hey!21
-Fatal error: Uncaught PDOException: SQLSTATE[23000]: 
-Integrity constraint violation: 19 FOREIGN KEY constraint 
-failed in C:\xampp\htdocs\feup-LTW\Project\src\database\messages.
-php:45 Stack trace: #0 C:\xampp\htdocs\feup-LTW\Project\src\database
-\messages.php(45): PDOStatement->execute(Array) #1 C:\xampp\htdocs\feup-LTW
-\Project\src\actions\action_message_add.php(20): addMessage('Hey!', '2', '1') 
-#2 {main} thrown in C:\xampp\htdocs\feup-LTW\Project\src\database\messages.php 
-on line 45 -->
-
-<!-- CREATE TABLE Message (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, message 
-TEXT NOT NULL, receiver INTEGER NOT NULL REFERENCES User (id) ON DELETE 
-CASCADE ON UPDATE CASCADE, sender INTEGER NOT NULL REFERENCES User (id) 
-ON DELETE CASCADE ON UPDATE CASCADE, date DATE CONSTRAINT "Post Date" DEFAULT 
-(date('now', '%d-%m-%Y'))) -->
