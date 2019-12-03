dba43199111f5a3181b37a37e456feeff65ff880
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-12-03 09:22:27 +0000

    click action on property comments

--- Project/src/templates/tpl_comments.php
+++ Project/src/templates/tpl_comments.php
@@ -5,21 +5,21 @@ include_once('../templates/tpl_common.php');
 function draw_comment($comment) {
     $image = getUserImagePath($comment['user_id']);?>
     <article class='comment'>
-        <img src=<?= getUserImagePath($comment['user_id'], 'SMALL') ?> alt="Image of <?= $image ?>">
-        <h3><?= $comment['name'] ?></h3>
-        <h6><?= $comment['date'] ?><?php draw_rating($comment['rating']); ?></h6> <!-- missing star image -->
-        <p><?= $comment['comment'] ?></p>
+		<a href="../profile/profile.php?id=<?=$comment['user_id']?>">
+			<img src=<?= getUserImagePath($comment['user_id'], 'SMALL') ?> alt="Image of <?= $image ?>">
+			<h3><?= $comment['name'] ?></h3>
+			<h6><?= $comment['date'] ?><?php draw_rating($comment['rating']); ?></h6> <!-- missing star image -->
+			<p><?= $comment['comment'] ?></p>
+		</a>
     </article>
 <?php }
 
 
 function draw_allComments($comments) { ?>
-  <section id="comments">
-    <h2>Comments</h2>
-    <?php foreach ($comments as $comment) {
-        draw_comment($comment);
-      } ?>
-  </section>
-<?php }
-
-?>
+	<section id="comments">
+		<h2>Comments</h2>
+		<?php foreach ($comments as $comment) {
+			draw_comment($comment);
+		} ?>
+	</section>
+<?php } ?>
