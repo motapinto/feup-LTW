482ea860f2c67165a7fa6cb7180d11241cb5f415
luis ramos <deco.luis@live.com.pt>
2019-12-08 16:41:17 +0000

    filter cities

--- Project/src/templates/tpl_filter.php
+++ Project/src/templates/tpl_filter.php
@@ -3,30 +3,40 @@ include_once('../database/listings.php');
 
 function draw_filters($filter) { 
     $max = getMaxPrice()['price_day'];
-    $min = getMinPrice()['price_day'];?>
+    $min = getMinPrice()['price_day'];
+    $cities = getCities();
+    ?>
     <aside class="filter">
         <form action="" method="get" class="filter-form">
         <h2> Custom Filter</h2>
-            <ul>
-                <li>	
-                    <label>
+            <section> 
+                <ul>
+                    <li>	
                         <input name="apartment" type="checkbox" class="filled-in" <?php if($filter === false || isset($_GET['apartment'])) { ?> checked <?php } ?>/>
                         <span>Apartment</span>
-                    </label>
-                </li>
-                <li>	
-                    <label>
+                    </li>
+                    <li>	
                         <input name="house" type="checkbox" class="filled-in" <?php if($filter === false || isset($_GET['house'])) { ?> checked <?php } ?>/>
                         <span>House</span>
-                    </label>
-                </li>
-            </ul>
-            <label> 
+                    </li>
+                </ul>
+            </section>
+            <section> 
                 <span>Price Range (per night)</span>
                 <div id="price-slider"></div>
                 <input type="hidden" name="price_min" id='price_min' value="<?=isset($_GET['price_min'])?$_GET['price_min']:0?>" max="<?=$max - 1?>" min="<?=$min?>">
                 <input type="hidden" name="price_max" id='price_max' value="<?=isset($_GET['price_max'])?$_GET['price_max']:$max?>" max="<?=$max?>" min="<?=$min + 1?>">
-            </label>
+            </section>
+
+            <section>
+                <span>City</span>
+                <select visible="6">
+                    <option selected value="%">Any</option>
+                    <?php foreach ($cities as $city) { ?>
+                        <option value="<?=$city?>"><?=$city?></option>
+                    <?php } ?>
+                </select>
+            </section>
 
             <button class="circular-button" style="background-color: teal;">Update Results</button>
         </form>
