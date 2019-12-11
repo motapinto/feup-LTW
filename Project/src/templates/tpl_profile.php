bb3b8ab551e7cc3c96c10372b290986cc45273d0
luis ramos <deco.luis@live.com.pt>
2019-12-11 17:26:43 +0000

    cancel rent js

--- Project/src/templates/tpl_profile.php
+++ Project/src/templates/tpl_profile.php
@@ -95,7 +95,6 @@
                     <?php if($canEditProfile) { 
                         if(strcmp($date_init_array[1].'-'.$date_init_array[2].'-'.$date_init_array[3], $now) > 0) { ?>
                             <input class="rent-id" type="hidden" value="<?=$rent['id']?>">
-                            <input class="owner" type="hidden" value="<?=$rent['id']?>">
                             <button class="cancel-button"> Cancel </button>
                         <?php }
                     } ?>
